17725433fa7d69803dbf7d948fee84f635c4a5ae
Marat Akhmetianov <marat.akhmetianov@gmail.com>
2020-09-21 11:09:08 +0200

    Update optimizations.sql

--- topic11/optimizations.sql
+++ topic11/optimizations.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS logs;
 CREATE TABLE logs (
 	created_at DATETIME NOT NULL,
 	table_name VARCHAR(100) NOT NULL,
-	id BIGINT(30) NOT NULL,
+	row_id BIGINT(30) NOT NULL,
 	name VARCHAR(100) NOT NULL
 ) ENGINE = ARCHIVE;
 
@@ -21,21 +21,21 @@ DROP TRIGGER IF EXISTS logs_products //
 CREATE TRIGGER logs_users AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
-	INSERT INTO logs (created_at, table_name, id, name)
+	INSERT INTO logs (created_at, table_name, row_id, name)
 	VALUES (NOW(), 'users', NEW.id, NEW.name);
 END //
 
 CREATE TRIGGER logs_catalogs AFTER INSERT ON catalogs
 FOR EACH ROW
 BEGIN
-	INSERT INTO logs (created_at, table_name, id, name)
+	INSERT INTO logs (created_at, table_name, row_id, name)
 	VALUES (NOW(), 'catalogs', NEW.id, NEW.name);
 END //
 
 CREATE TRIGGER logs_products AFTER INSERT ON products
 FOR EACH ROW
 BEGIN
-	INSERT INTO logs (created_at, table_name, id, name)
+	INSERT INTO logs (created_at, table_name, row_id, name)
 	VALUES (NOW(), 'products', NEW.id, NEW.name);
 END //
 
